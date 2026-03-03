eadaab477e9fbc7ae27d5ac69db577b967c9d45e
Jordqn <jordan.toulain@protonmail.com>
2026-03-03 23:15:01 +0100

    fix: Update login section to ensure full-width layout on screen

--- public/views/login.php
+++ public/views/login.php
@@ -1,4 +1,4 @@
-<section class="container flex flex-col items-center justify-center h-full">
+<section class="container flex flex-col items-center justify-center h-full w-screen m-auto">
     <form class="form w-2/3 max-w-100 m-auto grid gap-6">
         <div role="group" class="field">
             <label for="username">Nom d'utilisateur</label>
