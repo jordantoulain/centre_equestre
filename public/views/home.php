ddd7d69a5822307cc1de3bfded2e0bf394796728
Jordqn <jordan.toulain@protonmail.com>
2026-03-03 23:34:39 +0100

    feat: Added an image into the home view.

--- public/views/home.php
+++ public/views/home.php
@@ -1,4 +1,5 @@
 <section class="container mx-auto">
+    <img src="<?= asset(entryName: 'src/horse.svg') ?>" alt="Image de bienvenue" class="h-64 object-cover mb-8 mt-25 mx-auto">
     <h1 class="text-4xl font-bold text-center mb-8">Bienvenue au Centre Equestre</h1>
     <p class="text-lg text-center mb-12">Découvrez nos activités équestres, nos chevaux et notre équipe passionnée.</p>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-8"></div>
