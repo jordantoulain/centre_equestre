1e60468b4321c532b1c53eeea24a5b7156ee40dc
Jordqn <jordan.toulain@protonmail.com>
2026-03-06 00:07:14 +0100

    fix: Correct title spelling and add project note in home section

--- public/views/home.php
+++ public/views/home.php
@@ -1,7 +1,8 @@
 <section class="container mx-auto h-screen w-screen flex flex-col justify-center items-center">
     <img src="./images/horse.svg" alt="Image de bienvenue" class="h-64 object-cover mb-8 mt-25 mx-auto">
-    <h1 class="text-4xl font-bold text-center mb-8">Bienvenue au Centre Equestre</h1>
+    <h1 class="text-4xl font-bold text-center mb-8">Bienvenue au Centre Équestre ! 👋</h1>
     <p class="text-lg text-center mb-12">Découvrez nos activités équestres, nos chevaux et notre équipe passionnée.</p>
+    <p class="text-muted-foreground">Nb: Ceci est un projet factice à but éducatif dans le cadre d'un exercice de formation en BTS SIO.</p>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-8"></div>
 
     </div>
