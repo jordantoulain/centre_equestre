0c960312d57bbbaac28833ad434a50cacfa5ced2
Jordqn <jordan.toulain@protonmail.com>
2026-03-05 22:48:13 +0100

    fix: Remove unnecessary comment in environment loading function

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ if(empty($_ENV['APP_ENV'])){
         if (!file_exists($path)) return;
         $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         foreach ($lines as $line) {
-            if (strpos(trim($line), '#') === 0) continue; // Ignore les commentaires
+            if (strpos(trim($line), '#') === 0) continue;
             list($name, $value) = explode('=', $line, 2);
             $_ENV[trim($name)] = trim($value);
         }
