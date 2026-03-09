d0026d632353f6f3635cd56630e236056f8e1d86
Jordqn <jordan.toulain@protonmail.com>
2026-03-09 11:47:44 +0100

    fix: Adjust image visibility for better responsiveness in home view

--- app/features/home/views/home.php
+++ app/features/home/views/home.php
@@ -1,5 +1,6 @@
 <section class="container mx-auto h-screen w-screen flex flex-col justify-center items-center">
-    <img src="./images/horse.svg" alt="Image de bienvenue" class="h-64 object-cover mb-8 mt-25 mx-auto">
+    <img src="./images/horse.svg" alt="Image de bienvenue" class="block dark:hidden h-64 object-cover mb-8 mt-10 mx-auto">
+    <img src="./images/horse2.svg" alt="Image de bienvenue" class="hidden dark:block h-64 object-cover mb-8 mt-10 mx-auto">
     <h1 class="text-4xl font-bold text-center mb-8">Bienvenue au Centre Équestre ! 👋</h1>
     <p class="text-lg text-center mb-12">Découvrez nos activités équestres, nos chevaux et notre équipe passionnée.</p>
     <p class="text-muted-foreground">Nb: Ceci est un projet factice à but éducatif dans le cadre d'un exercice de formation en BTS SIO.</p>
